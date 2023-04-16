950c8248f903e8154462e613af1478b738c85bc0
abhannan1 <122967255+abhannan1@users.noreply.github.com>
2023-04-16 16:22:10 +0500

    fixed engine

--- connection/dbConfig.js
+++ connection/dbConfig.js
@@ -3,8 +3,8 @@ const mongoose  = require("mongoose")
 
 const connection = () => {
 mongoose.connect(process.env.db_Url, {useNewUrlParser:true , useUnifiedTopology:true, dbName:"TodoApp"})
-.then(()=>{
-    console.log("DB connected")
+.then((c)=>{
+    console.log(`DB connected with ${c.connection.host}`)
 })
 .catch((error)=>{
     console.log("DB connection failed:" + error.message)
